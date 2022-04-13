c2cbcfbe8062a97d53ceb19bb2bd7b41971ea4e0
prashantmagare <prashantmagare3010@gmail.com>
2022-04-13 19:12:03 +0530

    added:UC-1_Employee is Present Or Absent

--- Day-9/EmployeeWage/EmployeeWage.sh
+++ Day-9/EmployeeWage/EmployeeWage.sh
@@ -1,2 +1,12 @@
 
 echo "welcome to employee wage Computation Program"
+
+present=1
+absent=0
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $present ]
+then
+	echo "Employee present"
+else
+	echo "Employee absent"
+fi
