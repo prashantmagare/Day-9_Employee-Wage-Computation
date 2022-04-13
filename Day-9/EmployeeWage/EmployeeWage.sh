8a7bddd877e72676fc9977a37ff43c7f6ab5b27c
prashantmagare <prashantmagare3010@gmail.com>
2022-04-13 19:48:44 +0530

    added: UC-2_DailyEmployeeWage

--- Day-9/EmployeeWage/EmployeeWage.sh
+++ Day-9/EmployeeWage/EmployeeWage.sh
@@ -3,10 +3,16 @@ echo "welcome to employee wage Computation Program"
 
 present=1
 absent=0
+wagePerHour=20
+workingHr=8
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq $present ]
 then
 	echo "Employee present"
+	workingHr=8
 else
 	echo "Employee absent"
+	workingHr=0
 fi
+salary=$((wagePerHour*workingHr))
+echo "Daily employee wage : $salary"
